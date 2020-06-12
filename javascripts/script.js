23b9d7aaddd638d3b1e1b9c8eb7b66b86fab3bc9
helloworld101 <wbzamerica@gmail.com>
2020-06-12 15:13:32 -0700

    adding typewriter effect on the landing page

---
+++ javascripts/script.js
@@ -0,0 +1,13 @@
+var i = 0,
+    a = 0,
+    isBackspacing = false,
+    isParagraph = false;
+
+var intro = new Array(
+    "Hi, my name's Dwight Schrute.",
+    "I'm Assistant Regional Manager of Dudder Mifflin.",
+    "Welcome to my personal website.",
+    "For your convinience, I've broken it down into three parts:"
+);
+
+var speedForward = 100,
