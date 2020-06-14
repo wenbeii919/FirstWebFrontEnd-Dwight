230c9ac0e334857ca8e7a2c51e9812623eace099
helloworld101 <wbzamerica@gmail.com>
2020-06-14 15:45:29 -0700

    finishing up professional.html file

--- javascripts/script.js
+++ javascripts/script.js
@@ -1,5 +1,45 @@
-// $(function() {
-//     $('#button').click(function() {
-//         $("external link").attr({'href': 'https://www.youtube.com/watch?v=4HcxLnDaB_w'});
-//     });
-// })
+$(document).ready(function() {
+    $("#b1").hover(function() {
+        $("#b1").css("background-color", "yellowgreen");
+    },
+    function() {
+        $(this).css("background-color", "black");
+    });
+
+    $("#b2").hover(function() {
+        $("#b2").css("background-color", "yellowgreen");
+    },
+    function() {
+        $(this).css("background-color", "black");
+    });
+
+    $("#b3").hover(function() {
+        $("#b3").css("background-color", "yellowgreen");
+    },
+    function() {
+        $(this).css("background-color", "black");
+    });
+
+    $("#b4").hover(function() {
+        $("#b4").css("background-color", "yellowgreen");
+    },
+    function() {
+        $(this).css("background-color", "black");
+    });
+
+    $("#b1").click(function() {
+        $("#dm-salesman").css("color", "white");
+    });
+
+    $("#b2").click(function() {
+        $("#staples").css("color", "white");
+    });
+
+    $("#b3").click(function() {
+        $("#dm-acting-manager").css("color", "white");
+    });
+
+    $("#b4").click(function() {
+        $("#dm-manager").css("color", "white");
+    });
+});
